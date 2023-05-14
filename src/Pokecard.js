d46b4ca6caa1f531d368aa00c674b56d81ed548f
Tiela Black-Law <tiela.rose@gmail.com>
2023-05-14 13:25:53 -0700

    Use map instead of for loop, use fancier images

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,13 +1,21 @@
 function Pokecard(props) {
-  const { id, name, type, base_experience } = props;
-  const img_url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+  function pad_to_3_digits(num) {
+    return num.toLocaleString("en-US", {
+      minimumIntegerDigits: 3,
+      useGrouping: false
+    });
+  }
+
+  const { id, name, type, exp } = props;
+  const id_3_dig = pad_to_3_digits(id);
+  const img_url = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id_3_dig}.png`;
 
   return (
     <div className="Pokecard">
       <h3>{name}</h3>
       <img src={img_url} />
       <p>type: {type}</p>
-      <p>EXP: {base_experience}</p>
+      <p>EXP: {exp}</p>
     </div>
   );
 }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,12 +1,25 @@
 import Pokecard from "./Pokecard";
 
-function Pokedex(props) {
+function Pokedex() {
+  const poke_list = [
+    { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+    { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+    { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+    { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+    { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+    { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+    { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+    { id: 133, name: "Eevee", type: "normal", base_experience: 65 }
+  ];
+
+  const all_cards = poke_list.map((x) => (
+    <Pokecard id={x.id} name={x.name} type={x.type} exp={x.base_experience} />
+  ));
+
   return (
     <div className="Pokedex">
       <h1>Pokedex</h1>
-      <div className="Pokedex-cards">
-        <Pokecard id={1} name="test" type="water" base_experience={85} />
-      </div>
+      <div className="Pokedex-cards">{all_cards}</div>
     </div>
   );
 }
